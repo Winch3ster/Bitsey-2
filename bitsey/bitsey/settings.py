c363efb4ade7612d37badc4b92c8b9dd4a022571
Kingston Lee <kingstonlee96@gmail.com>
2023-12-10 18:14:03 +0800

    Disabled collectstatic

--- bitsey/bitsey/settings.py
+++ bitsey/bitsey/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vdqi_svffxjrobl8088l$ogni96!482gpup_!h9vpcli9os$cx'
+#SECRET_KEY = 'django-insecure-vdqi_svffxjrobl8088l$ogni96!482gpup_!h9vpcli9os$cx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
